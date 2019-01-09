4164668d890d8977deb279d8255574fbd5d7f20d
KitKat <katherine_nguyen7@yahoo.com>
2019-01-08 22:05:07 -0800

    testing connections

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,14 @@
 var express = require("express");
 
-var burger = require("../models/burgers.js");
-
 var router = express.Router();
+
+var burger = require("../models/burgers.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     burger.all(function(data) {
       var hbsObject = {
-        cats: data
+        burgers: data
       };
       console.log(hbsObject);
       res.render("index", hbsObject);
@@ -17,9 +17,9 @@ router.get("/", function(req, res) {
   
   router.post("/api/burgers", function(req, res) {
     burger.create([
-      "name", "sleepy"
+      "burger_name", "devoured"
     ], [
-      req.body.name, req.body.sleepy
+      req.body.burger_name, req.body.devoured
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
@@ -32,7 +32,7 @@ router.get("/", function(req, res) {
     console.log("condition", condition);
   
     burger.update({
-      sleepy: req.body.sleepy
+      devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
